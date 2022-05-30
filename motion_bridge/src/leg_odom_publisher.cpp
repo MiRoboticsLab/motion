bd7c4bf8db047ef2d16b184d29059657a220b406
田洪海 <tianhonghai@126.com>
2022-05-30 13:53:08 +0800

    [Modify]: add motion_bridge

---
+++ motion_bridge/src/leg_odom_publisher.cpp
@@ -0,0 +1,96 @@
+// Copyright (c) 2021 Beijing Xiaomi Mobile Software Co., Ltd. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+#include <memory>
+#include "motion_bridge/leg_odom_publisher.hpp"
+
+namespace cyberdog
+{
+namespace motion_bridge
+{
+LegOdomPublisher::LegOdomPublisher(const rclcpp::Node::SharedPtr node)
+: map_frame_("map"),
+  odom_frame_("odom"),
+  base_frame_("base_link")
+{
+  node_ = node;
+  leg_odom_publisher_ = node_->create_publisher<nav_msgs::msg::Odometry>("leg_odom", rclcpp::SystemDefaultsQoS());
+  tf2_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(node_);
+  lcm_ = std::make_shared<lcm::LCM>("udpm://239.255.76.67:7667?ttl=255");
+  lcm_->subscribe("global_to_robot", &LegOdomPublisher::OdomLCMCabllback, this);
+  std::thread{
+    [this](){
+      while (rclcpp::ok()) {
+        while (0 == this->lcm_->handleTimeout(1000) && rclcpp::ok()) {
+          ERROR("Cannot read LegOdomLCM from MR813");
+          if(ready_publish_) {
+            ready_publish_ = false;
+          }
+        }
+        if(!ready_publish_) {
+          ready_publish_ = true;
+          cv_.notify_one();
+        }
+      }
+    }
+  }.detach();
+  std::thread{
+    [this](){
+      while(rclcpp::ok()){
+        if (!ready_publish_) {
+          std::unique_lock<std::mutex> lk(mutex_);
+          cv_.wait(lk);
+        }
+        leg_odom_publisher_->publish(odom_);
+        tf2_broadcaster_->sendTransform(transform_stamped_);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+      }
+    }
+  }.detach();
+}
+
+void LegOdomPublisher::OdomLCMCabllback(const lcm::ReceiveBuffer *, const std::string &,
+    const localization_lcmt * msg)
+{
+
+  odom_.header.frame_id = transform_stamped_.header.frame_id = odom_frame_;
+  odom_.header.stamp = transform_stamped_.header.stamp =node_->get_clock()->now();
+  odom_.child_frame_id = transform_stamped_.child_frame_id = base_frame_;
+  odom_.pose.pose.position.x = transform_stamped_.transform.translation.x = msg->xyz[0];
+  odom_.pose.pose.position.y = transform_stamped_.transform.translation.y = msg->xyz[1];
+  odom_.pose.pose.position.z = transform_stamped_.transform.translation.y = msg->xyz[2];
+  tf2::Quaternion q;
+  q.setRPY(msg->rpy[0], msg->rpy[1], msg->rpy[2]);
+  odom_.pose.pose.orientation.x = transform_stamped_.transform.rotation.x = q.getX();
+  odom_.pose.pose.orientation.y = transform_stamped_.transform.rotation.y = q.getY();
+  odom_.pose.pose.orientation.z = transform_stamped_.transform.rotation.z = q.getZ();
+  odom_.pose.pose.orientation.w = transform_stamped_.transform.rotation.w = q.getW();
+  odom_.twist.twist.linear.x = msg->vxyz[0];
+  odom_.twist.twist.linear.y = msg->vxyz[1];
+  odom_.twist.twist.linear.z = msg->vxyz[2];
+  odom_.twist.twist.angular.x = msg->omegaBody[0];
+  odom_.twist.twist.angular.y = msg->omegaBody[1];
+  odom_.twist.twist.angular.z = msg->omegaBody[2];
+
+}
+
+void LegOdomPublisher::Spin()
+{
+  rclcpp::spin(node_);
+  rclcpp::shutdown();
+}
+
+}
+}
---
+++ motion_bridge/test/as_controller.cpp
@@ -0,0 +1,76 @@
+#include "lcm/heightmap_t.hpp"
+#include "lcm/lcm-cpp.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "grid_map_core/grid_map_core.hpp"
+#include "grid_map_msgs/msg/grid_map.hpp"
+#include "grid_map_ros/grid_map_ros.hpp"
+#include "cyberdog_common/cyberdog_log.hpp"
+#include "motion_bridge/elevation_bridge.hpp"
+namespace cyberdog
+{
+namespace motion_bridge
+{
+class AsController
+{
+public:
+  AsController()
+  {
+    node_.reset(new rclcpp::Node("as_controller"));
+    lcm_.reset(new lcm::LCM("udpm://239.255.76.67:7667?ttl=255"));
+    pub_ = node_->create_publisher<grid_map_msgs::msg::GridMap>(
+      "elevation_submap",
+      rclcpp::SystemDefaultsQoS());
+    lcm_->subscribe("local_heightmap", &AsController::LCMCallback, this);
+    std::thread{[this]() {
+        // while (0 == lcm_->handle()) {}}
+        while (rclcpp::ok()) {
+          while (0 == this->lcm_->handleTimeout(1000)) {
+            ERROR("Cannot read LCM from bridge");
+          }
+        }
+      }}.detach();
+    length_x_ = SIZE_X * RESOLUTION;
+    length_y_ = SIZE_Y * RESOLUTION;
+  }
+  ~AsController() {}
+  void Spin()
+  {
+    rclcpp::spin(node_);
+    rclcpp::shutdown();
+  }
+
+private:
+  void LCMCallback(const lcm::ReceiveBuffer *, const std::string &, const heightmap_t * msg)
+  {
+    grid_map::GridMap map;
+    map.setFrameId("map");
+    map.add(LAYER_ELEVATION);
+    grid_map::Position center(msg->robot_loc[0], msg->robot_loc[1]);
+    map.setGeometry(grid_map::Length(length_x_, length_y_), RESOLUTION, center);
+    grid_map::GridMapIterator iter(map); // NOTE 迭代器从左上角开始向下遍历，不同于SubmapIterator向右开始遍历。？
+    for (int8_t j = 0; j < SIZE_Y; ++j) {
+      for (int8_t i = 0; i < SIZE_X; ++i) {
+        map.at(LAYER_ELEVATION, *iter) = msg->map[i][j];
+        ++iter;
+      }
+    }
+    std::unique_ptr<grid_map_msgs::msg::GridMap> map_ros;
+    map_ros = grid_map::GridMapRosConverter::toMessage(map);
+    pub_->publish(*map_ros);
+  }
+  std::shared_ptr<lcm::LCM> lcm_;
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::Publisher<grid_map_msgs::msg::GridMap>::SharedPtr pub_;
+  double length_x_, length_y_;
+};
+}
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  LOGGER_MAIN_INSTANCE("AsController");
+  std::shared_ptr<cyberdog::motion_bridge::AsController> ac =
+    std::make_shared<cyberdog::motion_bridge::AsController>();
+  ac->Spin();
+}
