feff25b7d2fb299234482cf8766d0fcff088e4ab
田洪海 <tianhonghai@126.com>
2022-06-23 14:27:36 +0800

    [Add]: imu_bridge need debug

---
+++ motion_bridge/include/motion_bridge/imu_bridge.hpp
@@ -0,0 +1,59 @@
+// Copyright (c) 2021 Beijing Xiaomi Mobile Software Co., Ltd. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef MOTION_BRIDGE__IMU_BRIDGE_HPP_
+#define MOTION_BRIDGE__IMU_BRIDGE_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <lcm/lcm-cpp.hpp>
+#include <tf2_ros/create_timer_ros.h>
+#include <cyberdog_common/cyberdog_log.hpp>
+#include <string>
+#include <memory>
+#include "motion_action/motion_macros.hpp"
+#include "protocol/lcm/microstrain_lcmt.hpp"
+#include "protocol/lcm/robot_control_response_lcmt.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+namespace cyberdog
+{
+namespace motion
+{
+constexpr int8_t SIZE_X = 80;
+constexpr int8_t SIZE_Y = 80;
+constexpr float RESOLUTION = 0.05;
+constexpr const char * LAYER_ELEVATION = "elevation";
+constexpr const char * LAYER_TRAVERSABILITY = " ";
+class ImuBridge
+{
+public:
+  explicit ImuBridge(const rclcpp::Node::SharedPtr node);
+  ~ImuBridge() {}
+  void Spin();
+
+private:
+  void ReadLcm(
+    const lcm::ReceiveBuffer *, const std::string &,
+    const microstrain_lcmt * msg);
+    // const robot_control_response_lcmt * msg);
+  rclcpp::Node::SharedPtr node_;
+  std::shared_ptr<lcm::LCM> lcm_subscribe_instance_;
+  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_ {nullptr};
+  sensor_msgs::msg::Imu::SharedPtr imu_ros_data_ {nullptr};
+  microstrain_lcmt imu_lcm_data_;
+  robot_control_response_lcmt response_lcm_data_;
+  std::thread topic_publish_thread_, lcm_handle_thread_;
+  std::string imu_frame_;
+};  // class ImuBridge
+}  // namespace motion
+}  // namespace cyberdog
+#endif  // MOTION_BRIDGE__ELEVATION_BRIDGE_HPP_
---
+++ motion_bridge/src/imu_bridge.cpp
@@ -0,0 +1,90 @@
+// Copyright (c) 2021 Beijing Xiaomi Mobile Software Co., Ltd. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+#include <memory>
+#include "motion_bridge/imu_bridge.hpp"
+
+namespace cyberdog
+{
+namespace motion
+{
+ImuBridge::ImuBridge(const rclcpp::Node::SharedPtr node)
+: imu_frame_("imu")
+{
+  node_ = node;
+  imu_pub_ = node->create_publisher<sensor_msgs::msg::Imu>("imu", rclcpp::SystemDefaultsQoS());
+  lcm_subscribe_instance_ = std::make_shared<lcm::LCM>(kBirdgeSubscribeURL);
+  lcm_subscribe_instance_->subscribe("external_imu", &ImuBridge::ReadLcm, this);
+  // lcm_subscribe_instance_ = std::make_shared<lcm::LCM>(kActionSubscibeURL);
+  // lcm_subscribe_instance_->subscribe("robot_control_response", &ImuBridge::ReadLcm, this);
+  imu_ros_data_.reset(new sensor_msgs::msg::Imu);
+
+  lcm_handle_thread_ =
+    std::thread(
+    [this]() {
+      while (rclcpp::ok()) {
+        while (0 == this->lcm_subscribe_instance_->handleTimeout(kAcitonLcmReadTimeout)) {
+          ERROR("Cannot read LCM from MR813");
+        }
+      }
+    });
+  lcm_handle_thread_.detach();
+
+  topic_publish_thread_ =
+  std::thread(
+  [this]() {
+    while (rclcpp::ok()) {
+      imu_pub_->publish(*imu_ros_data_);
+      std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+  });
+  topic_publish_thread_.detach();
+}
+
+void ImuBridge::Spin()
+{
+  rclcpp::spin(node_);
+  rclcpp::shutdown();
+}
+
+void ImuBridge::ReadLcm(
+  const lcm::ReceiveBuffer *, const std::string &,
+  const microstrain_lcmt * msg)
+  // const robot_control_response_lcmt * msg)
+{
+  imu_ros_data_->header.frame_id = imu_frame_;
+  imu_ros_data_->header.stamp = node_->get_clock()->now();
+  // imu_ros_data_->linear_acceleration.x = msg->acc[0];
+  // imu_ros_data_->linear_acceleration.y = msg->acc[1];
+  // imu_ros_data_->linear_acceleration.z = msg->acc[2];
+  // imu_ros_data_->angular_velocity.x = msg->omega[0];
+  // imu_ros_data_->angular_velocity.y = msg->omega[1];
+  // imu_ros_data_->angular_velocity.z = msg->omega[2];
+  // imu_ros_data_->orientation.x = msg->quat[0];
+  // imu_ros_data_->orientation.y = msg->quat[1];
+  // imu_ros_data_->orientation.z = msg->quat[2];
+  // imu_ros_data_->orientation.w = msg->quat[3];
+}
+}  // namespace motion
+}  // namespace cyberdog
+
+int main(int argc, char const *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::Node::SharedPtr node(new rclcpp::Node("test_imu_bridege"));
+  cyberdog::motion::ImuBridge ib(node);
+  ib.Spin();
+  return 0;
+}
