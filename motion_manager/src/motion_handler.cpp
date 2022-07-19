de084fe8226b7881773e7664286d4f36d0f2009a
田洪海 <tianhonghai@126.com>
2022-07-19 10:23:59 +0800

    [Fix]: pose control definitively duration fault in servo end frame
    CARPO-259

--- motion_manager/src/motion_handler.cpp
+++ motion_manager/src/motion_handler.cpp
@@ -175,6 +175,7 @@ void MotionHandler::PoseControlDefinitively()
   MotionResultSrv::Response::SharedPtr response(new MotionResultSrv::Response);
   request->motion_id = MotionIDMsg::POSECONTROL_DEFINITIVELY;
   request->pos_des = std::vector<float>{0.0, 0.0, 0.2};
+  request->duration = 200;
   // action_ptr_->Execute(request);
   ExecuteResultCmd(request, response);
 }
