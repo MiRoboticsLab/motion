062c696e96b5057a7f9dd9ebac825f2dde1c380c
田洪海 <tianhonghai@126.com>
2022-07-27 11:53:33 +0800

    [Modify]: cancel tof points arrangement

--- motion_utils/scripts/head_tof_pcl_publisher.py
+++ motion_utils/scripts/head_tof_pcl_publisher.py
@@ -124,37 +124,39 @@ class MinimalSubscriber(Node):
             y_array[idx] = round(r_array[idx]*sin_array_scale[7-int(idx/8)]/math.sqrt(r2_array[idx]),4)
             x_array[idx], y_array[idx], z_array[idx] = np.dot(right_R, np.array([x_array[idx], y_array[idx], z_array[idx]])) + right_t
         right_points = np.vstack((np.asarray(x_array),np.asarray(y_array),np.asarray(z_array))).T
-        points = np.array([[]]*3).T
-        for row in range(0,8):
-            left_tmp_points = np.array([[]]*3).T
-            right_tmp_points = np.array([[]]*3).T
-            for col in range(0,8):
-                #  Raw:
-                #  *  *  *  *  *  * 55 63   *  *  *  *  *  *  *  0  
-                #  *  *  *  *  *  *  * 62   *  *  *  *  *  *  *  1 
-                #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  2 
-                #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  * 
-                #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  * 
-                #  2  *  *  *  *  *  *  *   *  *  *  *  *  *  *  * 
-                #  1  *  *  *  *  *  * 57  62  *  *  *  *  *  *  * 
-                #  0  *  *  *  *  *  * 56  63 55  *  *  *  *  *  7 
-                #                           
-                #                        | x
-                #                   y    |
-                #                    ————   
-                # Concatenated:  
-                #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  * 127  
-                #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  1 
-                #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  2 
-                #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  * 
-                #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  * 
-                #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  * 
-                # 16  *  *  *  *  *  *  *   *  *  *  *  *  *  * 31 
-                #  0  1  2  *  *  *  *  7   8  9  *  *  *  *  * 15               
-                left_tmp_points = np.concatenate((left_tmp_points, left_points[[col*8+row], :]), axis=0)
-                right_tmp_points  = np.concatenate((right_tmp_points, right_points[[(8-col)*8-1-row], :]), axis=0)
-            tmp_points = np.concatenate((left_tmp_points, right_tmp_points), axis=0)
-            points = np.concatenate((points, tmp_points), axis=0)
+        # arrange points
+        # points = np.array([[]]*3).T
+        # for row in range(0,8):
+        #     left_tmp_points = np.array([[]]*3).T
+        #     right_tmp_points = np.array([[]]*3).T
+        #     for col in range(0,8):
+        #         #  Raw:
+        #         #  *  *  *  *  *  * 55 63   *  *  *  *  *  *  *  0  
+        #         #  *  *  *  *  *  *  * 62   *  *  *  *  *  *  *  1 
+        #         #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  2 
+        #         #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  * 
+        #         #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  * 
+        #         #  2  *  *  *  *  *  *  *   *  *  *  *  *  *  *  * 
+        #         #  1  *  *  *  *  *  * 57  62  *  *  *  *  *  *  * 
+        #         #  0  *  *  *  *  *  * 56  63 55  *  *  *  *  *  7 
+        #         #                           
+        #         #                        | x
+        #         #                   y    |
+        #         #                    ————   
+        #         # Concatenated:  
+        #         #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  * 127  
+        #         #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  1 
+        #         #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  2 
+        #         #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  * 
+        #         #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  * 
+        #         #  *  *  *  *  *  *  *  *   *  *  *  *  *  *  *  * 
+        #         # 16  *  *  *  *  *  *  *   *  *  *  *  *  *  * 31 
+        #         #  0  1  2  *  *  *  *  7   8  9  *  *  *  *  * 15               
+        #         left_tmp_points = np.concatenate((left_tmp_points, left_points[[col*8+row], :]), axis=0)
+        #         right_tmp_points  = np.concatenate((right_tmp_points, right_points[[(8-col)*8-1-row], :]), axis=0)
+        #     tmp_points = np.concatenate((left_tmp_points, right_tmp_points), axis=0)
+        #     points = np.concatenate((points, tmp_points), axis=0)
+        points = np.concatenate((left_points, right_points), axis=0)
         pcd = point_cloud(points, "robot")
         self.head_pcd_publisher.publish(pcd)
         return
