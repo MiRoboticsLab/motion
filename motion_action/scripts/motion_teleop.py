933e1163a3404a806d231039958bb542bc3c1d76
田洪海 <tianhonghai@126.com>
2022-09-23 11:38:53 +0800

    [Modify]: add trigger in motion teleop
    1. 在键盘控制节点中加入开关
    
    Signed-off-by: 田洪海 <tianhonghai@126.com>

--- motion_action/scripts/motion_teleop.py
+++ motion_action/scripts/motion_teleop.py
@@ -13,9 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from concurrent.futures import thread
+from doctest import register_optionflag
 import os
 import select
 import sys, tty, termios, getopt
+from time import sleep
+import threading
 # from typing import Protocol
 import rclpy
 
@@ -24,7 +28,7 @@ from geometry_msgs.msg import Twist
 from protocol.msg import MotionServoCmd
 from protocol.msg import MotionID
 from rclpy.qos import QoSProfile
-
+from std_srvs.srv import SetBool
 
 msg = """
 Control Your CyberDog!
@@ -66,6 +70,8 @@ speedBindings={
         'c':( 1.0, 0.9),
           }
 
+triggered = True
+
 def getKey():
     tty.setraw(sys.stdin.fileno())
     rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
@@ -85,18 +91,24 @@ def switch_run():
 def keep_idle():
     return
 
-def main(argv):
-    if len(argv) == 1:
-        end = 0
-    else:
-        end = argv[1]
+def service_callback(request:SetBool.Request, response:SetBool.Response):
+    global triggered
+    triggered = request.data
+    # print(triggered)
+    response.success = True
+    return response
+
+def main(end=True, auto=True):
     global settings
+    global triggered
+    # print(auto)
+    # print(triggered)
     settings = termios.tcgetattr(sys.stdin)
     rclpy.init()
     qos = QoSProfile(depth=10)
     node = rclpy.create_node('teleop_keyboard')
     pub = node.create_publisher(MotionServoCmd, 'motion_servo_cmd', qos)
-
+    service = node.create_service(SetBool, 'enable_teleop', service_callback)
     status = 0
     count = 0
     motion_id = 0
@@ -115,81 +127,87 @@ def main(argv):
     control_turn = 0.0
 
     try:
-      print(msg)
-      print(vels(speed, turn))
-      while(1):
-          motion_id = MotionID.WALK_ADAPTIVELY
-          key = getKey()
-          if key in moveBindings.keys():
-              x = moveBindings[key][0]
-              y = moveBindings[key][2]
-              th = moveBindings[key][1]
-              count = 0
-          elif key in speedBindings.keys():
-              speed = speed * speedBindings[key][0]
-              speed_y = speed_y * speedBindings[key][0]
-              turn = turn * speedBindings[key][1]
-              count = 0
-              print(vels(speed,turn))
-              if (status == 14):
-                  print(msg)
-              status = (status + 1) % 15
-          elif key == ' ' or key == 'k' :
-              x = 0.0
-              y = 0.0
-              th = 0.0
-              control_speed = 0.0
-              control_speed_y = 0.0
-              control_turn = 0.0
-          else:
-              count = count + 1
-              if count > 4:
-                  x = 0.0
-                  y = 0.0
-                  th = 0.0
-              if (key == '\x03'):
-                  break
+        print(msg)
+        print(vels(speed, turn))
+        while(1):
+            rclpy.spin_once(node=node, timeout_sec=0)
+            # print(auto)
+            # print(triggered)
+            if not triggered:
+                sleep(0.1)
+                continue
+            motion_id = MotionID.WALK_ADAPTIVELY
+            key = getKey()
+            if key in moveBindings.keys():
+                x = moveBindings[key][0]
+                y = moveBindings[key][2]
+                th = moveBindings[key][1]
+                count = 0
+            elif key in speedBindings.keys():
+                speed = speed * speedBindings[key][0]
+                speed_y = speed_y * speedBindings[key][0]
+                turn = turn * speedBindings[key][1]
+                count = 0
+                print(vels(speed,turn))
+                if (status == 14):
+                    print(msg)
+                status = (status + 1) % 15
+            elif key == ' ' or key == 'k' :
+                x = 0.0
+                y = 0.0
+                th = 0.0
+                control_speed = 0.0
+                control_speed_y = 0.0
+                control_turn = 0.0
+            else:
+                count = count + 1
+                if count > 4:
+                    x = 0.0
+                    y = 0.0
+                    th = 0.0
+                if (key == '\x03'):
+                    break
 
-          target_speed = speed * x
-          target_speed_y = speed_y * y
-          target_turn = turn * th
+            target_speed = speed * x
+            target_speed_y = speed_y * y
+            target_turn = turn * th
 
-          if target_speed > control_speed:
-              control_speed = min( target_speed, control_speed + 0.02 )
-          elif target_speed < control_speed:
-              control_speed = max( target_speed, control_speed - 0.02 )
-          else:
-              control_speed = target_speed
+            if target_speed > control_speed:
+                control_speed = min( target_speed, control_speed + 0.02 )
+            elif target_speed < control_speed:
+                control_speed = max( target_speed, control_speed - 0.02 )
+            else:
+                control_speed = target_speed
 
-          if target_speed_y > control_speed_y:
-              control_speed_y = min( target_speed_y, control_speed_y + 0.02 )
-          elif target_speed_y < control_speed_y:
-              control_speed_y = max( target_speed_y, control_speed_y - 0.02 )
-          else:
-              control_speed_y = target_speed_y
+            if target_speed_y > control_speed_y:
+                control_speed_y = min( target_speed_y, control_speed_y + 0.02 )
+            elif target_speed_y < control_speed_y:
+                control_speed_y = max( target_speed_y, control_speed_y - 0.02 )
+            else:
+                control_speed_y = target_speed_y
 
-          if target_turn > control_turn:
-              control_turn = min( target_turn, control_turn + 0.1 )
-          elif target_turn < control_turn:
-              control_turn = max( target_turn, control_turn - 0.1 )
-          else:
-              control_turn = target_turn
+            if target_turn > control_turn:
+                control_turn = min( target_turn, control_turn + 0.1 )
+            elif target_turn < control_turn:
+                control_turn = max( target_turn, control_turn - 0.1 )
+            else:
+                control_turn = target_turn
 
-          cmd = MotionServoCmd()
-          cmd.motion_id = motion_id; 
-        #   cmd.mode = mode; cmd.gait_id = gait_id; cmd.life_count = life_count + 1
-          cmd.vel_des.fromlist([control_speed, control_speed_y, control_turn])
-          cmd.pos_des.fromlist([0.0, 0.0, 0.2])
-          cmd.step_height.fromlist([0.05, 0.05])
-          cmd.value = 2
-        #   if key in moveBindings.keys():
-          pub.publish(cmd)
+            cmd = MotionServoCmd()
+            cmd.motion_id = motion_id; 
+            #   cmd.mode = mode; cmd.gait_id = gait_id; cmd.life_count = life_count + 1
+            cmd.vel_des.fromlist([control_speed, control_speed_y, control_turn])
+            cmd.pos_des.fromlist([0.0, 0.0, 0.2])
+            cmd.step_height.fromlist([0.05, 0.05])
+            cmd.value = 2
+            #   if key in moveBindings.keys():
+            pub.publish(cmd)
 
     except Exception as e:
         print(e)
 
     finally:
-        if end == 'end':
+        if end:
             cmd = MotionServoCmd()
             cmd.cmd_type = MotionServoCmd.SERVO_END
             # cmd.motion_id = MotionID.FORCECONTROL_RELATIVEYLY
@@ -200,4 +218,14 @@ def main(argv):
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    end = None
+    argv = sys.argv[1:]
+ 
+    try:
+        opts, args = getopt.getopt(argv, "e:")  # 短选项模式
+    except:
+        print("Error")
+    for opt, arg in opts:
+        if opt in ['-e']:
+            end = arg
+    main(end)
