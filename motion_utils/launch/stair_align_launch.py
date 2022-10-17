0f433179a2bb8a2de358ffcd6c20b785793290ff
马世成 <mashicheng@xiaomi.com>
2022-10-17 19:30:53 +0800

    [Fix]:fix error reported by pipeline
    Signed-off-by: mashicheng<mashicheng@xiaomi.com>

--- motion_utils/launch/stair_align_launch.py
+++ motion_utils/launch/stair_align_launch.py
@@ -13,9 +13,10 @@
 # limitations under the License.
 import os
 from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
