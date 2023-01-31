cae41775ebe59c9b927dc0ba41393126c816f797
田洪海 <tianhonghai@126.com>
2023-01-31 15:24:28 +0800

    [Modify]: keep same step when stop servo
    Signed-off-by: 田洪海 <tianhonghai@126.com>

--- motion_manager/src/motion_handler.cpp
+++ motion_manager/src/motion_handler.cpp
@@ -266,7 +266,7 @@ void MotionHandler::WalkStand(const MotionServoCmdMsg::SharedPtr & last_servo_cm
   MotionResultSrv::Request::SharedPtr request(new MotionResultSrv::Request);
   // request->motion_id = last_servo_cmd->motion_id;
   // request->step_height = last_servo_cmd->step_height;
-  // request->value = last_servo_cmd->value;
+  request->value = last_servo_cmd->value;
   // request->duration = 500;
   request->motion_id = MotionIDMsg::WALK_STAND;
   request->step_height = last_servo_cmd->step_height;
